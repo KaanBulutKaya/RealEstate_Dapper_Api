5b9242bdc9d623e14d918ca9a279c8bd244999fd
Soloi <Soloi@KAANBULUT>
2024-07-16 15:15:11 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        void CreateCategory(CreateCategoryDto categoryDto);
+
+        void DeleteCategory(int id);
+
+        void UptadeCategory(UpdateCategoryDto categoryDTo);
+
+        Task<GetByIDCategoryDto> GetCategory(int id);
+    }
+}
